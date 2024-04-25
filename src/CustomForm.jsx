028823d6e2c0f80e985a974a3ffb7d0ea477e874
sumit962 <sumit.adsul@vivacommerce.com>
2024-04-25 10:25:33 +0530

    added chekout extention

--- src/CustomForm.jsx
+++ src/CustomForm.jsx
@@ -251,6 +251,7 @@ const CustomForm = () => {
   const handleShippingChange = (event) => {
     // console.log(event.target.value);
     setWhoPaysShipping(event.target.value);
+    //call azure function to update the product prices
   };
 
   const handleSellersShipperChange = (e) => {
@@ -514,6 +515,89 @@ const CustomForm = () => {
     //   });
   };
 
+
+  async function consignmentUpdateTriggered(extensionService, cartId, data) {
+    console.log('consignments changed', data);
+    //compareConsignments(data.payload.consignments, data.payload.previousConsignments);
+
+    showLoadingIndicator(extensionService);
+    //post message to parent window - hide continue button
+    window.top.postMessage("hide-checkout-shipping-continue", "https://sellars-absorbent-materials-sandbox-1.mybigcommerce.com");
+
+
+    //perform price update operations
+
+    try {
+
+      await requestCartPriceUpdate(cartId);
+    } catch (e) {
+      console.log("Error in requestCartPriceUpdate");
+    }
+
+    //sleep for 3 seconds
+    await sleep(1000);
+    //post message to parent window - show continue button
+    hideLoadingIndicator();
+    window.top.postMessage("show-checkout-shipping-continue", "https://sellars-absorbent-materials-sandbox-1.mybigcommerce.com");
+    //window.top.postMessage("checkout-shipping-next-step", "https://sellars-absorbent-materials-sandbox-1.mybigcommerce.com");
+  }
+
+  async function requestCartPriceUpdate(cartId) {
+    //make fetch call to  http://localhost:7071/api/updateProductPrices with cartID as a post json
+    //test with California and Florida
+    const cartUpdate = await fetch('http://localhost:7071/api/updateProductPrices', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        checkoutId: cartId,
+        fedex:{
+          accountNumber:"CUST-101"
+        }
+      })
+    }).then(response => {
+      console.log(response);
+      return response.json();
+    });
+
+    console.log(cartUpdate);
+
+  }
+
+  function compareConsignments(consignments, previousConsignments) {
+    let changed = false;
+    consignments.forEach((consignment) => {
+      const { id, shippingAddress: { country, stateOrProvinceCode } } = consignment;
+      //const shippingOptionId = consignment?.selectedShippingOption?.id;
+
+      if (previousConsignments.length === 0) {
+        changed = true;
+      } else {
+        const prevConsignment = previousConsignments.find(prev => prev.id === id);
+        const previousCountry = prevConsignment.shippingAddress.country;
+        const previousStateOrProvinceCode = prevConsignment.shippingAddress.stateOrProvinceCode;
+      //  const previousShippingOptionId = prevConsignment?.selectedShippingOption?.id;
+
+        if (country !== previousCountry) {
+          console.log(`️🔄 Consignment #${id} shipping country change: ${previousCountry} -> ${country}.`);
+          changed = true;
+        }
+        if (stateOrProvinceCode !== previousStateOrProvinceCode) {
+          console.log(`️🔄 Consignment #${id} shipping state change: ${previousStateOrProvinceCode} -> ${stateOrProvinceCode}.`);
+          changed = true;
+        }
+        // if (shippingOptionId !== previousShippingOptionId) {
+        //   console.log(`️🔄 Consignment #${id} shipping option change: ${previousShippingOptionId} -> ${shippingOptionId}.`);
+        //   changed = true;
+        // }
+        
+      }
+    });
+    return changed;
+  }
+
+
   useEffect(() => {
     checkoutKitLoader.load("extension").then(async function (module) {
       console.log("Checkout loader - extension");
@@ -541,10 +625,18 @@ const CustomForm = () => {
         "EXTENSION:CONSIGNMENTS_CHANGED",
         async (data) => {
           console.log("inside consignments chnaged listener");
-          console.log(
-            data?.payload?.consignments,
-            data?.payload?.previousConsignments
-          );
+          //console.log(data?.payload?.consignments,data?.payload?.previousConsignments);
+
+
+          const priceUpdateNeeded = compareConsignments(data?.payload?.consignments, data?.payload?.previousConsignments);
+          if (priceUpdateNeeded) {
+            console.log("Consignment updated, need to trigger price update.")
+            consignmentUpdateTriggered(extensionService, cartId, data);
+          } else {
+            console.log("Key Consignment fields(country, state, shipping option) not updated, no need to trigger price update.")
+          }
+          //call azure function to update the product prices
+
         }
       );
 
