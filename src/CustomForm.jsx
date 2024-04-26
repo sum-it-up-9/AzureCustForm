a1c69970723d4c4a670deec4c48af6431d879e5e
Suraj Kumar Modi <suraj.modi@vivacommerce.com>
2024-04-26 13:07:30 +0530

    Testing JWT

--- src/CustomForm.jsx
+++ src/CustomForm.jsx
@@ -493,7 +493,7 @@ const CustomForm = () => {
   //   extensionService.post({ type: ExtensionCommandType.ReloadCheckout });
   // }
   async function customerJWT(apiAccountClientId) {
-    let resource = `/customer/current.jwt?app_client_id=${apiAccountClientId}`;
+    let resource = `https://vivacommerce-b2b-demo-i9.mybigcommerce.com/customer/current.jwt?app_client_id=${apiAccountClientId}`;
     return fetch(resource)
     .then(response => {
       if(response.status === 200) {
