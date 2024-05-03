8556adf439408dbf2dc77062a10dc2331bbcf875
sumit962 <sumit.adsul@vivacommerce.com>
2024-05-03 14:59:22 +0530

    first commit

--- src/CustomForm.jsx
+++ src/CustomForm.jsx
@@ -259,7 +259,7 @@ const CustomForm = () => {
         }
         else{
             setSelectedShipper(initialShipper);
-            //isDisplayingAccountNumber(initialShipper);
+            setIsDisplayingAccountNumber(initialShipper);
             console.log('dhekc sgipperL ',initialShipper);
         }
     },[initialShipper]);
