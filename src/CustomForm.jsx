9b29ce3488c4022ddeeb73cd5ed9ba798c0bfe1a
sumit962 <sumit.adsul@vivacommerce.com>
2024-05-03 15:00:50 +0530

    first commit

--- src/CustomForm.jsx
+++ src/CustomForm.jsx
@@ -896,6 +896,7 @@ const CustomForm = () => {
                                         label="Account Number"
                                         variant="outlined"
                                         required
+                                        value={accountNumber}
                                         onChange={(e) => {
                                             setAccountNumber(e.target.value);
                                         }}
