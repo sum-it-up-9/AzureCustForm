11aad4ec3e3824d5777a7183500de71a4051fbe2
saps-sengupta <saptarshi.sengupta@vivacommerce.com>
2024-05-08 13:01:43 +0530

    This reverts commit b01871b5ff73ffce3d7ecefb097d38db5e24695f. Sumit's last commit

--- src/CustomForm.jsx
+++ src/CustomForm.jsx
@@ -213,7 +213,7 @@ const ExtensionCommandType = {
 async function sendMessage() {
     window.top.postMessage(
         "hide-checkout-shipping-continue",
-        "https://vivacommerce-b2b-demo-i9.mybigcommerce.com"
+        "https://sellars-absorbent-materials-sandbox-1.mybigcommerce.com"
     );
 }
 
@@ -467,7 +467,7 @@ const CustomForm = () => {
         //post message to parent window - hide continue button
         window.top.postMessage(
             "hide-checkout-shipping-continue",
-            "https://vivacommerce-b2b-demo-i9.mybigcommerce.com"
+            "https://sellars-absorbent-materials-sandbox-1.mybigcommerce.com"
         );
 
         //call azure function to update the product prices
@@ -489,7 +489,7 @@ const CustomForm = () => {
         });
         window.top.postMessage(
             "show-checkout-shipping-continue",
-            "https://vivacommerce-b2b-demo-i9.mybigcommerce.com"
+            "https://sellars-absorbent-materials-sandbox-1.mybigcommerce.com"
         );
 
     };
@@ -799,7 +799,7 @@ const CustomForm = () => {
         //post message to parent window - hide continue button
         window.top.postMessage(
             "hide-checkout-shipping-continue",
-            "https://vivacommerce-b2b-demo-i9.mybigcommerce.com"
+            "https://sellars-absorbent-materials-sandbox-1.mybigcommerce.com"
         );
         try {
             await UpdateCartPrice(cartId);
@@ -816,12 +816,108 @@ const CustomForm = () => {
 
         window.top.postMessage(
             "show-checkout-shipping-continue",
-            "https://vivacommerce-b2b-demo-i9.mybigcommerce.com"
+            "https://sellars-absorbent-materials-sandbox-1.mybigcommerce.com"
         );
     };
 
     useEffect(() => {
-        checkoutKitLoader.load("extension");
+        checkoutKitLoader.load("extension").then(async function (module) {
+            // console.log("Checkout loader - extension");
+            const params = new URL(document.location).searchParams;
+
+            // console.log("params: ", params);
+
+            const extensionId = params.get("extensionId");
+            // console.log("this is exctention id: ", extensionId);
+            cartId = params.get("cartId");
+
+            async function fetchData() {
+                try {
+                  const response = await fetch(`http://localhost:3000/getCartMetafields/${cartId}`);
+                  
+                  if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                  }
+                  
+                  const data = await response.json();
+                //  console.log(data); // Process the data received from the API
+                  if(data?.data[0]?.value){
+                    metafields=JSON.parse(data?.data[0]?.value);
+                  }
+                 
+                //  console.log('this is metafields',metafields);
+                  setFlag(prev=>!prev);
+                } catch (error) {
+                  console.error('There was a problem with the fetch operation:', error);
+                }
+            }
+              
+              // Call the function to fetch the data
+            fetchData();
+
+            
+
+            //console.log("this is card id: ", cartId);
+            setCheckoutid(cartId);
+
+            const parentOrigin = params.get("parentOrigin");
+            //  console.log("this is parentOrigin: ", parentOrigin);
+
+            extensionService = await module.initializeExtensionService({
+                extensionId,
+                parentOrigin,
+                taggedElementId: "container",
+            });
+
+            // console.log("extentionService: ", extensionService);
+
+            extensionService.addListener(
+                "EXTENSION:CONSIGNMENTS_CHANGED",
+                async (data) => {
+                    console.log("inside consignments chnaged listener");
+                    showLoadingIndicator();
+                   
+                    //console.log(data?.payload?.consignments,data?.payload?.previousConsignments);
+                   
+                    //post message to parent window - hide continue button
+                    window.top.postMessage(
+                        "hide-checkout-shipping-continue",
+                        "https://sellars-absorbent-materials-sandbox-1.mybigcommerce.com"
+                    );
+
+
+
+                    const priceUpdateNeeded = compareConsignments(
+                        data?.payload?.consignments,
+                        data?.payload?.previousConsignments
+                    );
+                    if (priceUpdateNeeded) {
+                        console.log("Consignment updated, need to trigger price update.");
+                        consignmentUpdateTriggered(extensionService, cartId, data);
+                        console.log("reload checkout with updated price.");
+                        extensionService.post({
+                            type: ExtensionCommandType.ReloadCheckout,
+                        });
+
+                    } else {
+                        console.log(
+                            "Key Consignment fields(country, state, shipping option) not updated, no need to trigger price update."
+                        );
+                     
+                       
+  
+                    }
+                    await sleep(1000);
+                    hideLoadingIndicator();
+                    window.top.postMessage(
+                      "show-checkout-shipping-continue",
+                      "https://vivacommerce-b2b-demo-i9.mybigcommerce.com"
+                  );
+
+                }
+            );
+        });
+
         // Cleanup function
         return () => {
             // Cleanup code if necessary
